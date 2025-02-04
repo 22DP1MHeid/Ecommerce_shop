1f0eca17a70e192254d7379483a55ccd8caff40e
22DP1MHeid <131352976+22DP1MHeid@users.noreply.github.com>
2025-02-04 21:41:47 +0000

    added about and home pages

---
+++ clothing_shop/resources/views/home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+import MainHome from "@/components/MainHome.vue";
+</script>
+
+<template>
+  <main>
+    <NavBar/>
+    <MainHome/>
+    <Footer/>
+  </main>
+</template>
